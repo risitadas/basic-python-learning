2d3fbe1472e0e83eea384eacba55bd377fb0bd32
risitadas <92712417+risitadas@users.noreply.github.com>
2022-06-28 14:46:09 +0530

    git innitialize

---
+++ Creating a Python Package Using Setuptools.py
@@ -0,0 +1,15 @@
+'''
+
+Creating a Python Package Using Setuptools
+
+    from setuptools import setup
+    pip install wheel
+    python setup.py bdist_wheel
+    python setup.py sdist bdist_wheel
+    pip install package_name
+    Import package_name
+
+
+'''
+
+
